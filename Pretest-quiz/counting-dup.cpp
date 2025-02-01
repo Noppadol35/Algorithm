5e1d3c7c8db5e44ca68b9bb2bec1a45b334fb7a1
Noppadol35 <s6504062620191@email.kmutnb.ac.th>
2025-02-02 00:31:51 +0700

    Refactor quick_sort and binary_search functions; remove unused parameters and improve readability

--- Pretest-quiz/counting-dup.cpp
+++ Pretest-quiz/counting-dup.cpp
@@ -2,95 +2,77 @@
 
 using namespace std;
 
-int partition(int arr[], int l, int r){
+int x = 0;
+
+int partition(int arr[], int l, int r)
+{
     int pivot = arr[r];
     int i = l - 1;
-    for(int j = l; j <= r; j++){
-        if(arr[j] < pivot){
+    for (int j = l; j <= r; j++)
+    {
+        if (arr[j] < pivot)
+        {
             i++;
             swap(arr[i], arr[j]);
         }
     }
-    swap(arr[i+1], arr[r]);
+    swap(arr[i + 1], arr[r]);
     return i + 1;
 }
 
-void quick_sort(int arr[], int l , int r){
-    if(l < r){
+void quick_sort(int arr[], int l, int r)
+{
+    x = x + 1;
+    if (l < r)
+    {
         int p = partition(arr, l, r);
         quick_sort(arr, l, p - 1);
         quick_sort(arr, p + 1, r);
     }
 }
 
-int binary_search_first(int a[], int l, int r, int k, int index)
+int binary_search_first(int a[], int l, int r, int k)
 {
 
     int mid = (l + r) / 2;
 
     while (l <= r)
     {
+        x = x + 1;
         // เจอ
         if (k == a[mid])
         {
-            index = mid;
-            return binary_search_first(a, l, mid - 1, k, index);
+            return mid;
         }
 
         if (k > a[mid])
         {
-            return binary_search_first(a, mid + 1, r, k, index);
+            return binary_search_first(a, mid + 1, r, k);
         }
         else
         {
-            return binary_search_first(a, l, mid - 1, k, index);
+            return binary_search_first(a, l, mid - 1, k);
         }
     }
-    return index;
+    return -1;
 }
 
-int binary_search_last(int a[], int l, int r, int k, int index)
-{
-
-    int mid = (l + r) / 2;
-
-    while (l <= r)
-    {
-        // เจอ
-        if (k == a[mid])
-        {
-            index = mid;
-            return binary_search_last(a, mid + 1, r, k, index);
-        }
-
-        if (k > a[mid])
-        {
-            return binary_search_last(a, mid + 1, r, k, index);
-        }
-        else
-        {
-            return binary_search_last(a, l, mid - 1, k, index);
-        }
-    }
-    return index;
-}
 
 int main()
 {
 
-    int n = 10; //int n = sizeof(arr)/sizeof(arr[0]);
-    int a[] = {11, 11, 6, 4, 2, 5, 4, 4, 2, 6};
-    int target = 4;
+    // int n = 4; //int n = sizeof(arr)/sizeof(arr[0]);
+    // int arr[] = {1,-2,1,-2};
+    // int target = -2;
 
-    quick_sort(a, 0, n - 1);
+    int n = 10;
+    int arr[] = {60, 70, 70, 60, 60, 70, 70, 70, 70, 50};
+    int target = 70;
 
-    for(int i = 0; i < n; i++){
-        cout << a[i] << " ";
-    }
-    cout << endl;
+    quick_sort(arr, 0, n - 1);
 
-    int F = binary_search_first(a, 0, n - 1, target, -1);
-    int L = binary_search_last(a, 0, n - 1, target, -1);
+    // int F = binary_search_first(arr, 0, n - 1, target, -1);
+    // int L = binary_search_last(arr, 0, n - 1, target, -1);
 
-    cout << (L - F) + 1 << endl;
+    cout << x << endl;
 }

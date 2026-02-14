b5d8a86aaff1575a412a9a9dbb76c7ea04c96abb
Noppadol P. <noppadol.p.promtas@gmail.com>
2026-02-14 16:32:30 +0700

    commit: pre-quiz-1

---
+++ Mid-term/2-2568/pre-quiz/summation.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+long long calculateSumation(long long n){
+    return (n*(n + 1)) / 2;
+}
+
+
+long long recuresiveBinaryFunction(long long low, long long high, long long target){
+    if(high < low){
+        return 0;
+    } 
+    long long mid = low + (high - low) / 2;
+    long long curentMid = calculateSumation(mid);
+
+    if(curentMid == target){
+        return mid;
+    } else if (curentMid > target) {
+        return recuresiveBinaryFunction(low, mid-1, target);
+    } else {
+        return recuresiveBinaryFunction(mid + 1, high, target);
+    }
+
+}
+int main(){
+    long long target;
+    cin >> target;
+    long long result = recuresiveBinaryFunction(1, 2000000000, target);
+    cout << result;
+    return 0;
+}
+
+// logn long int target;
+    // cin >> target;
+    // int sum_temp = 0;
+    // int sum = 0;
+    // int i =0;
+    // while(sum_temp != target){
+    //     sum++;
+    //     sum_temp += sum;
+    //     i++;
+    // }
+    // cout << sum_temp;
+    // cout << endl;
+    // cout << i;

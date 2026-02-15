7bec997fd3d2f247cf55de413120d69ce93964e8
Noppadol P. <noppadol.p.promtas@gmail.com>
2026-02-15 07:22:37 +0700

    commit: max-sum

--- Mid-term/2-2568/pre-quiz/max-sub-arraysum.cpp
+++ Mid-term/2-2568/pre-quiz/max-sub-arraysum.cpp
@@ -1,41 +1,45 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int maxSubArray(int arr[], int l, int m, int r){
+int maxsubarraycross(int a[], int l, int mid, int r)
+{
     int sum_left = 0;
-    int min_left = -99999;
+    int min_left = -9999;
 
-    for (int i = m; i >= l; i--)
-    {
-        sum_left = sum_left + arr[i];
+    for (int i = mid; i >= l; i--){
+        sum_left = sum_left + a[i];
         min_left = max(min_left, sum_left);
     }
 
     int sum_right = 0;
-    int min_right = -99999;
+    int min_right = -9999;
 
-    for (int i = m + 1; i <= r; i++)
-    {
-        sum_right = sum_right + arr[i];
+    for (int i = mid + 1; i <= r; i++){
+        sum_right = sum_right + a[i];
         min_right = max(min_right, sum_right);
     }
 
     return min_left + min_right;
 }
 
-int maxSubArrayRec(int arr[], int l, int r){
-    if(l == r)return arr[l];
+int maxsubarray(int arr[], int l, int r)
+{
+    if (l == r){
+        return arr[l];
+    }
     int mid = l + (r - l) / 2;
-    
-    return max(maxSubArrayRec(arr, l, mid),
-            max(maxSubArrayRec(arr, mid + 1, r),
-                maxSubArray(arr, l, mid, r)));
+    return max(maxsubarray(arr, l, mid),
+    max(maxsubarray(arr, mid + 1, r),
+    maxsubarraycross(arr, l, mid, r)));
 }
 
-int main(){
-    int arr[] = {16, -25, 2, -54, 36, 9 ,-12, 66};
+int main()
+{
+    int arr[] = {16, -25, 2, -54, 36, 9, -12, 66};
     int n = sizeof(arr) / sizeof(arr[0]);
-    
-    maxSubArrayRec(arr, 0, n-1);
+    int result = maxsubarray(arr, 0, n - 1);
+
+    cout << result << " ";
     return 0;
 }
